c8b4ed75513578c46343ddb95440a0cec91d2add
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-07-25 15:34:01 -0400

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Gabriel Marquez on 2023-07-25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
